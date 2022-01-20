f8f84c97f70cf6c1d1e602af36d5af85b09518b6
murphycui <1224066169@qq.com>
2022-01-20 21:53:18 +0800

    bloom效果测试

---
+++ Assets/Scripts/Bloom.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bloom : PostEffectsBase {
+
+	public Shader bloomShader;
+	private Material bloomMaterial = null;
+	public Material material 
+    {  
+		get 
+        {
+			bloomMaterial = CheckShaderAndCreateMaterial(bloomShader, bloomMaterial);
+			return bloomMaterial;
+		}  
+	}
+
+	public int iterCount = 5;
+	public float blurSpread = 0.5f;
+
+	public int downSample = 2;
+
+	public float lumiThreshold = 0.6f;
+
+	void OnRenderImage (RenderTexture src, RenderTexture dest) 
+    {
+		if (material != null) 
+        {
+			material.SetFloat("_LuminanceThreshold", lumiThreshold);
+
+			int rtW = src.width/downSample;
+			int rtH = src.height/downSample;
+			
+			RenderTexture buffer0 = RenderTexture.GetTemporary(rtW, rtH, 0);
+			buffer0.filterMode = FilterMode.Bilinear;
+			
+			Graphics.Blit(src, buffer0, material, 0);
+			
+			for (int i = 0; i < iterCount; i++) 
+            {
+                RenderTexture buffer1 = RenderTexture.GetTemporary(rtW, rtH, 0);
+				material.SetFloat("_BlurSize", 1.0f + i * blurSpread);
+								
+				Graphics.Blit(buffer0, buffer1, material, 1);
+				
+				RenderTexture.ReleaseTemporary(buffer0);
+
+				buffer0 = buffer1;
+				buffer1 = RenderTexture.GetTemporary(rtW, rtH, 0);
+				
+				Graphics.Blit(buffer0, buffer1, material, 2);
+				
+				RenderTexture.ReleaseTemporary(buffer0);
+
+				buffer0 = buffer1;
+			}
+
+			material.SetTexture("_Bloom", buffer0);  
+			Graphics.Blit(src, dest, material, 3);  
+
+			RenderTexture.ReleaseTemporary(buffer0);
+		} 
+        else Graphics.Blit(src, dest);
+	}
+}
---
+++ Assets/Scripts/GaussianBlur.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GaussianBlur : PostEffectsBase 
+{
+
+	public Shader gaussianBlurShader;
+	private Material gaussianBlurMaterial = null;
+
+	public Material material 
+	{  
+		get 
+		{
+			gaussianBlurMaterial = CheckShaderAndCreateMaterial(gaussianBlurShader, gaussianBlurMaterial);
+			return gaussianBlurMaterial;
+		}  
+	}
+	public int iterations = 3;
+	
+	public float blurSpread = 0.6f;
+	
+	public int downSample = 2;
+	
+	void OnRenderImage (RenderTexture src, RenderTexture dest) 
+	{
+		if (material != null) 
+		{
+			int rtW = src.width / downSample;
+			int rtH = src.height / downSample;
+
+			RenderTexture buffer0 = RenderTexture.GetTemporary(rtW, rtH, 0);
+			buffer0.filterMode = FilterMode.Bilinear;
+
+			Graphics.Blit(src, buffer0);
+
+			for (int i = 0; i < iterations; i++) 
+			{
+				material.SetFloat("_BlurSize", 1.0f + i * blurSpread);
+
+				RenderTexture buffer1 = RenderTexture.GetTemporary(rtW, rtH, 0);
+
+				Graphics.Blit(buffer0, buffer1, material, 0);
+
+				RenderTexture.ReleaseTemporary(buffer0);
+				buffer0 = buffer1;
+				buffer1 = RenderTexture.GetTemporary(rtW, rtH, 0);
+
+				Graphics.Blit(buffer0, buffer1, material, 1);
+
+				RenderTexture.ReleaseTemporary(buffer0);
+				buffer0 = buffer1;
+			}
+
+			Graphics.Blit(buffer0, dest);
+			RenderTexture.ReleaseTemporary(buffer0);
+		} 
+		else Graphics.Blit(src, dest);
+	}
+}
